2ba604fac9cac2de73720ed9aab2f80456c0a336
Mari Törmälä <maritormala@Johans-MacBook-Pro.local>
2019-10-08 19:01:52 +0200

    hej L!

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Whoop! {{name}}
+    </h1>
   </div>
 </template>
+<script>
+export default {
+  data: ()=> ({
+    name: "anna", 
+    age: 1,
+  })
+}
+</script>
+<style scoped>
+h1 {
+  color:red;
+} 
+</style>
